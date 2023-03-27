bf701449e4e217c2ee782fda3edc5472f66223ad
USMANKHALID-GH <loftyusman@gmail.com>
2023-03-27 22:48:52 +0300

    repo

---
+++ src/main/java/com/usman/forum/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.usman.forum.repository;
+
+import com.usman.forum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
