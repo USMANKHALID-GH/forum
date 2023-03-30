6620d2b011908752c7aff72a3e844f2f85daa124
USMANKHALID-GH <loftyusman@gmail.com>
2023-03-30 12:19:49 +0300

    AnswerRepository

--- src/main/java/com/usman/forum/repository/AnswerRepository.java
+++ src/main/java/com/usman/forum/repository/AnswerRepository.java
@@ -3,10 +3,21 @@ package com.usman.forum.repository;
 import com.usman.forum.model.Answers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answers,Long> {
 
+    @Query("select count(a.question) from Answers  a  where a.question.Id=:id")
+    Long  numberOfAnsweredQuestion(@Param("id") Long id);
+
+    @Query("from Answers  a  where a.question.Id=:id")
+    List<Answers> findAllAnswersByQuestionID(@Param("id") Long id);
 
 }
