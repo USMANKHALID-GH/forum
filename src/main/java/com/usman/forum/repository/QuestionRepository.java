06a11dd4592594f98247154c9136fef6686b5674
USMANKHALID-GH <loftyusman@gmail.com>
2023-04-03 15:50:18 +0300

    questionRepository

--- src/main/java/com/usman/forum/repository/QuestionRepository.java
+++ src/main/java/com/usman/forum/repository/QuestionRepository.java
@@ -6,12 +6,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Questions,Long> {
 
     Page<Questions> findQuestionByTitleContaining(String search, Pageable pageable);
+
+    @Query("from Questions  q  where q.category.name like %:search%")
+    Page<Questions> findQuestionByCat(@Param("search") String search, Pageable pageable);
 
 
 }
