cf384e6c53de768474c9178ef23e57799ec09b45
USMANKHALID-GH <loftyusman@gmail.com>
2023-04-03 15:48:58 +0300

    SERVICE

--- src/main/java/com/usman/forum/service/CategoryService.java
+++ src/main/java/com/usman/forum/service/CategoryService.java
@@ -1,4 +1,10 @@
 package com.usman.forum.service;
 
+import com.usman.forum.model.Questions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CategoryService {
+
+    Page<Questions>  findQuestionByCat(String search, Pageable pageable);
 }
--- src/main/java/com/usman/forum/service/Implementation/CategoryImp.java
+++ src/main/java/com/usman/forum/service/Implementation/CategoryImp.java
@@ -1,8 +1,20 @@
 package com.usman.forum.service.Implementation;
 
+import com.usman.forum.model.Questions;
+import com.usman.forum.repository.QuestionRepository;
 import com.usman.forum.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CategoryImp implements CategoryService {
+   private final  QuestionRepository questionRepository;
+
+    @Override
+    public Page<Questions> findQuestionByCat(String search, Pageable pageable) {
+        return questionRepository.findQuestionByCat(search,pageable);
+    }
 }
