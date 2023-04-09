75f1705d8cf0e4a0ad1b57bef356b9a18308d486
USMANKHALID-GH <loftyusman@gmail.com>
2023-04-09 16:53:34 +0300

    security

--- src/main/java/com/usman/forum/service/UserService.java
+++ src/main/java/com/usman/forum/service/UserService.java
@@ -1,6 +1,7 @@
 package com.usman.forum.service;
 
 
+import com.usman.forum.dto.AuthenticationResponse;
 import com.usman.forum.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,10 @@ import org.springframework.data.domain.Pageable;
 
 
 public interface UserService {
+    AuthenticationResponse register(User user);
+
+
+    AuthenticationResponse authenticate(User user);
     void saveUser(User user);
 
     void updateUser(Long id, User user);
