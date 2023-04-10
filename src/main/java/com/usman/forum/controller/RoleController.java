5ca2ad2f51785a3998435b893d61dad833c3a3bd
USMANKHALID-GH <loftyusman@gmail.com>
2023-04-10 06:54:29 +0300

    security

--- src/main/java/com/usman/forum/controller/RoleController.java
+++ src/main/java/com/usman/forum/controller/RoleController.java
@@ -8,10 +8,7 @@ import com.usman.forum.model.Role;
 import com.usman.forum.service.RoleService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -24,5 +21,12 @@ public class RoleController {
     public ResponseEntity<BaseResponseDto> saveRole(@RequestBody RoleDto roleDto){
           service.saveRole(mapper.toEntity(roleDto));
         return ResponseEntity.ok(BaseResponseDto.builder().message("User saved successfully").build());}
+
+
+    @PostMapping("/{userId}/{roleId}/")
+    public  ResponseEntity<BaseResponseDto>  asignRole(@PathVariable("userId") Long userId, @PathVariable("roleId") Long roleId){
+        service.asignRole(userId, roleId);
+        return ResponseEntity.ok(BaseResponseDto.builder().message("Role asigned").build());
+    }
 
 }
