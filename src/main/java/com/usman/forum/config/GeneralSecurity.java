d689608d05aa38847cf6a183f868c1095d28c739
USMANKHALID-GH <loftyusman@gmail.com>
2023-04-13 15:57:58 +0300

    security

--- src/main/java/com/usman/forum/config/GeneralSecurity.java
+++ src/main/java/com/usman/forum/config/GeneralSecurity.java
@@ -13,12 +13,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.List;
+
 
 @AllArgsConstructor
 @Configuration
